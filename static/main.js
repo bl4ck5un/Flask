988846f1c62d7b9fc53633125782bd454a5aed80
Fan Zhang <bl4ck5unxx@gmail.com>
2015-11-10 20:42:18 -0500

    update: refinement

--- static/main.js
+++ static/main.js
@@ -1,8 +1,11 @@
 $(function(){
-  $('.message .close').on('click', function() {
-    // $(this).closest('.message').transition('fade');
-    $(this).closest('.message').fadeOut();
-  });
+  $('.message .close')
+    .on('click', function() {
+      $(this)
+        .closest('.message')
+        .transition('fade')
+      ;
+    });
 
-  $('#top-message').delay(2000).fadeOut();
+  // $('#top-message').delay(2000).fadeOut();
 });
